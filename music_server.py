eb4098801f9f9e2a8998d0d22fd20fb302fcfb35
Rohan Pooniwala <rohan.pooniwala@gmail.com>
2017-02-11 23:56:58 +0530

    Update music_server.py

--- music_server.py
+++ music_server.py
@@ -21,7 +21,7 @@ def get_ip():
 				break
 			except:
 				continue
-	return ""
+	return ip
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # bind the socket to a public host, and a well-known port
